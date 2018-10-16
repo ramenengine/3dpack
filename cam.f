5ae8ae31ea477984406342677e9801d84c0b273f
Unknown <roger.levy@gmail.com>
2018-10-15 23:53:47 -0400

    rename pan-to to pan-towards

--- cam.f
+++ cam.f
@@ -3,8 +3,8 @@ depend ramen/lib/v2d.f
 : posxz@  pos >x @  pos >z @ ; 
 : posxz!  pos >z !  pos >x ! ; 
 
-: tilt-to  ( obj dist -- n ) >r  's pos >y @  pos >y @ -  r> swap  angle ;
-: pan-to  ( obj -- n ) >{ posxz@ }  posxz@  2-  -1 1 2*  angle 90 + ;
+: tilt-towards  ( obj dist -- n ) >r  's pos >y @  pos >y @ -  r> swap  angle ;
+: pan-towards  ( obj -- n ) >{ posxz@ }  posxz@  2-  -1 1 2*  angle 90 + ;
 
 : orbitted ( obj ang dist -- x y )  vec  rot >{ posxz@ }  2+ ;
 : situate  ( obj ang dist -- )   orbitted  posxz! ;
